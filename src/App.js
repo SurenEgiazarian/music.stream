293a1ced7e9286ad76a553cfa29a511975471b31
SurenEgiazarian <126580601+SurenEgiazarian@users.noreply.github.com>
2023-04-01 16:07:19 +0400

    Revert "task2"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import "./App.css";
-import NavBlock from "./NavBlock";
-import SidebarBlock from "./SidebarBlock";
-import BarBlock from "./BarBlock"
-import CenterBlock from "./CenterBlock";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="wrapper">
-      <div className="container">
-        <div className="main">
-          <NavBlock />
-          <CenterBlock />
-          <SidebarBlock />
-        </div>
-        <BarBlock />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/PlayerControl.js
+++
@@ -1,9 +0,0 @@
-function PlayerControl(props) {
-  return(
-    <div className={props.itemClass}>
-        <img className={props.imgClass} src={props.imgSrc} alt={props.imgAlt} />
-    </div>
-  );
-}
-
-export default PlayerControl;
